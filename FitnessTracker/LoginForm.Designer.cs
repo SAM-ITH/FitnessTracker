ae3bf989017e651fa9bdc3ab9650c36724e8de46
samith wijesinghe <samithwijesinha@gmail.com>
2023-07-03 10:35:22 +0530

    add login form design and logic

---
+++ FitnessTracker/LoginForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace FitnessTracker
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.wellcomeLbl = new System.Windows.Forms.Label();
+            this.userNameLbl = new System.Windows.Forms.Label();
+            this.passwordLbl = new System.Windows.Forms.Label();
+            this.usernameTxt = new System.Windows.Forms.TextBox();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.loginBtn = new System.Windows.Forms.Button();
+            this.loginErrorMessage = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.loginErrorMessage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // wellcomeLbl
+            // 
+            this.wellcomeLbl.AutoSize = true;
+            this.wellcomeLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.wellcomeLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(35)))), ((int)(((byte)(5)))));
+            this.wellcomeLbl.Location = new System.Drawing.Point(287, 97);
+            this.wellcomeLbl.Name = "wellcomeLbl";
+            this.wellcomeLbl.Size = new System.Drawing.Size(246, 39);
+            this.wellcomeLbl.TabIndex = 0;
+            this.wellcomeLbl.Text = "Hi!, Wellcome";
+            this.wellcomeLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // userNameLbl
+            // 
+            this.userNameLbl.AutoSize = true;
+            this.userNameLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userNameLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(35)))), ((int)(((byte)(5)))));
+            this.userNameLbl.Location = new System.Drawing.Point(124, 219);
+            this.userNameLbl.Name = "userNameLbl";
+            this.userNameLbl.Size = new System.Drawing.Size(98, 20);
+            this.userNameLbl.TabIndex = 1;
+            this.userNameLbl.Text = "User Name";
+            // 
+            // passwordLbl
+            // 
+            this.passwordLbl.AutoSize = true;
+            this.passwordLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordLbl.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(35)))), ((int)(((byte)(5)))));
+            this.passwordLbl.Location = new System.Drawing.Point(124, 284);
+            this.passwordLbl.Name = "passwordLbl";
+            this.passwordLbl.Size = new System.Drawing.Size(86, 20);
+            this.passwordLbl.TabIndex = 2;
+            this.passwordLbl.Text = "Password";
+            // 
+            // usernameTxt
+            // 
+            this.usernameTxt.Location = new System.Drawing.Point(386, 218);
+            this.usernameTxt.Name = "usernameTxt";
+            this.usernameTxt.Size = new System.Drawing.Size(162, 20);
+            this.usernameTxt.TabIndex = 3;
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Location = new System.Drawing.Point(386, 283);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.PasswordChar = '*';
+            this.passwordTxt.Size = new System.Drawing.Size(162, 20);
+            this.passwordTxt.TabIndex = 4;
+            // 
+            // loginBtn
+            // 
+            this.loginBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(63)))), ((int)(((byte)(35)))), ((int)(((byte)(5)))));
+            this.loginBtn.Location = new System.Drawing.Point(386, 366);
+            this.loginBtn.Name = "loginBtn";
+            this.loginBtn.Size = new System.Drawing.Size(85, 35);
+            this.loginBtn.TabIndex = 5;
+            this.loginBtn.Text = "Login";
+            this.loginBtn.UseVisualStyleBackColor = true;
+            this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
+            // 
+            // loginErrorMessage
+            // 
+            this.loginErrorMessage.ContainerControl = this;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(234)))), ((int)(((byte)(211)))));
+            this.ClientSize = new System.Drawing.Size(808, 595);
+            this.Controls.Add(this.loginBtn);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.usernameTxt);
+            this.Controls.Add(this.passwordLbl);
+            this.Controls.Add(this.userNameLbl);
+            this.Controls.Add(this.wellcomeLbl);
+            this.Name = "LoginForm";
+            this.Text = "Fitness tracker";
+            ((System.ComponentModel.ISupportInitialize)(this.loginErrorMessage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label wellcomeLbl;
+        private System.Windows.Forms.Label userNameLbl;
+        private System.Windows.Forms.Label passwordLbl;
+        private System.Windows.Forms.TextBox usernameTxt;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.ErrorProvider loginErrorMessage;
+    }
+}
+
---
+++ FitnessTracker/LoginForm.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FitnessTracker
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {
+            // check the username and password validation
+            if (ValidateChildren(ValidationConstraints.Enabled))
+            {
+                string userName = usernameTxt.Text.Trim();
+                string password = passwordTxt.Text.Trim();
+                /* if () 
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("password or username you entered is incorrect", "Sorry");
+                }
+                */
+            }
+        }
+
+        // validate the username text field 
+        private void usernameTxt_validate(object sender, CancelEventArgs estop) 
+        {
+            if (string.IsNullOrWhiteSpace(usernameTxt.Text))
+            {
+                estop.Cancel = true;
+                usernameTxt.Focus();
+                loginErrorMessage.SetError(usernameTxt, "username can not be empty");
+            }
+            else 
+            {
+                estop.Cancel = false;
+                loginErrorMessage.SetError(usernameTxt, string.Empty);
+            }
+        }
+
+        private void password_validate(object seder, CancelEventArgs estop)
+        {
+            if (string.IsNullOrWhiteSpace(passwordTxt.Text))
+            {
+                estop.Cancel = true;
+                passwordTxt.Focus();
+                loginErrorMessage.SetError(passwordTxt, "password can not be emplty");
+            }
+            else 
+            {
+                estop.Cancel = false;
+                loginErrorMessage.SetError(passwordTxt, string.Empty);
+            }
+        }
+
+
+    }
+}
