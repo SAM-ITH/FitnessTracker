4f42f67399c7152f533a5368e36f5dd26218880e
samith wijesinghe <samithwijesinha@gmail.com>
2023-07-04 15:29:27 +0530

    add workout details to context

---
+++ FitnessTracker/Context/workoutDetails.cs
@@ -0,0 +1,48 @@
+﻿using FitnessTracker.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Context
+{
+        public static class workoutDetails
+        {
+            private static Hashtable _workouts;
+
+            public static void InitializeWorkoutDetails()
+            {
+                _workouts = new Hashtable();
+            }
+
+            public static void CreateWorkout(Workout workout)
+            {
+                var userName = userDetails.CurrentProfile.UserName;
+                if (_workouts.ContainsKey(userName))
+                {
+                    // get workout list.
+                    var workoutList = (List<Workout>)_workouts[userName];
+                    workoutList.Add(workout);
+                    _workouts[userName] = workoutList;
+                }
+                else
+                {
+                    List<Workout> workouts = new List<Workout> { workout };
+                    _workouts.Add(userName, workouts);
+                }
+            }
+
+            public static List<Workout> GetWorkouts()
+            {
+                var userName = userDetails.CurrentProfile.UserName;
+                if (_workouts.ContainsKey(userName))
+                {
+                    return (List<Workout>)_workouts[userName];
+                }
+
+                return new List<Workout>();
+            }
+        }
+}
