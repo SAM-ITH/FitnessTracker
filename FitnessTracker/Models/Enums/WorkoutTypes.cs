f6a01cc003e0abf1932e0506b2caaa010f5566e9
samith wijesinghe <samithwijesinha@gmail.com>
2023-07-04 15:29:52 +0530

    add new service file and ennums

---
+++ FitnessTracker/Models/Enums/WorkoutTypes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Models.Enums
+{
+    public enum WorkoutTypes
+    {
+        running = 1,
+        walking = 2,
+        pushups = 3,
+        crunches = 4,
+        squats = 5,
+        plank = 6, 
+        cycling = 7,
+        lunges = 8,
+        benchpress =9,
+        shoulderpress = 10,
+    }
+}
---
+++ FitnessTracker/Services/WorkoutService.cs
@@ -0,0 +1,23 @@
+﻿using FitnessTracker.Context;
+using FitnessTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Services
+{
+    public class WorkoutService
+    {
+        public void CreateWorkout(Workout workout)
+        {
+            workoutDetails.CreateWorkout(workout);
+        }
+
+        public List<Workout> GetWorkouts()
+        {
+            return workoutDetails.GetWorkouts();
+        }
+    }
+}
